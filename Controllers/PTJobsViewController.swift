cf523b2c9119ac3266d3767e2e21e197c1c58e85
muneeba1 <mkhatoo1@mail.depaul.edu>
2017-07-12 14:21:57 -0500

    imported XML/Alamofire and initialized vars

--- Controllers/PTJobsViewController.swift
+++ Controllers/PTJobsViewController.swift
@@ -8,11 +8,26 @@
 
 import Foundation
 import UIKit
+import Alamofire
+import CheatyXML
 
 class PTJobsViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var url: String = "http://api.indeed.com/ads/apisearch?publisher=2752372751835619&q=java&l=austin%2C+tx&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2"
+        
+        Alamofire.request(url).validate().responseData(completionHandler: { (response) in
+            //print(response.result.value)
+            
+            let data: CXMLParser! = CXMLParser(data: response.data)
+            //print(data)
+            
+            var object = JobPost(data: data)
+            
+        })
     }
-
+    
+    
 }
--- Models/JobPost.swift
+++ Models/JobPost.swift
@@ -8,26 +8,23 @@
 
 import Foundation
 import UIKit
+import CheatyXML
 
 class JobPost: NSObject {
     
-    var jobName: String
-    var compName: String
-    var location: String
-    var compensation: Float
-    
-    var resp: String
-    var qualifications: String
-    
-    init(jobTitle: Date, compName: String, location: String, compensation: Float, resp: String, qualifications: String)
+    var jobName: String?
+    var compName: String?
+    var location: String?
+    var about: String?
+
+
+    init(data: CXMLParser)
     {
-        self.jobName = ""
-        self.compName = compName
-        self.location =  location
-        self.compensation = compensation
-        
-        self.resp = resp
-        self.qualifications = qualifications
+        self.jobName = data["results"]["result"][0]["jobtitle"].string
+        self.compName = data["results"]["result"][0]["company"].string
+        self.location =  data["results"]["result"][0]["formattedLocation"].string
+        self.about = data["results"]["result"][0]["snippet"].string
+
     }
     
 }
